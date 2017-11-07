52aa544fc2f6a64c64b31b4abe03a3b3d198c3e9
Administrator <252539783@qq.com>
2017-11-07 21:28:52 +0800

    nio封装

--- app/src/main/java/com/iqiyi/liquanfei_sx/vpnt/tools/NIOHandler.java
+++ app/src/main/java/com/iqiyi/liquanfei_sx/vpnt/tools/NIOHandler.java
@@ -2,13 +2,13 @@ package com.iqiyi.liquanfei_sx.vpnt.tools;
 
 import android.util.Log;
 
-import com.iqiyi.liquanfei_sx.vpnt.service.ServerService;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.CancelledKeyException;
 import java.nio.channels.Channel;
 import java.nio.channels.ClosedChannelException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -22,16 +22,17 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * Created by Administrator on 2017/11/7.
  */
 
-public abstract class NIOHandler<T> {
+public abstract class NIOHandler<T extends NIOHandler.AttachmentObj> {
 
     private Selector mSelector;
     private Queue<Key> prepareRegister = new ConcurrentLinkedQueue<>();
+    private ByteBufferPool mBufferPool=ByteBufferPool.getDefault();
 
     public NIOHandler() throws IOException {
         mSelector=Selector.open();
     }
 
-    public void register(AbstractSelectableChannel channel,int ops,T o)
+    public void register(SocketChannel channel,int ops,T o)
     {
         prepareRegister.add(new Key(channel,ops,o));
     }
@@ -67,14 +68,15 @@ public abstract class NIOHandler<T> {
         while (keys.hasNext()) {
             SelectionKey key = keys.next();
             keys.remove();
-            SocketChannel channel = (SocketChannel) key.channel();
-            ServerService.TCPStatus status = (ServerService.TCPStatus) key.attachment();
+            SocketChannel channel =  (SocketChannel)key.channel();
+            T obj = (T) key.attachment();
             try {
                 if (key.isConnectable()) {
                     Log.e("xx", "key connect");
                     if (channel.isConnectionPending()) {
                         try {
                             if (channel.finishConnect()) {          //connect
+                                afterConnect(obj);
                                 key.interestOps(key.interestOps() | SelectionKey.OP_READ);
                             }
                         } catch (IOException e) {
@@ -82,32 +84,127 @@ public abstract class NIOHandler<T> {
                         }
                     }
                 }
+
                 if (key.isWritable()) {
                         //write
+                    //handleWrite(obj);
+
+                    if (!obj.mReadyWrite.isEmpty()) {
+                        write:
+                        while (obj.mReadyWrite.peek() != null) {
+                            WriteEntry se =(WriteEntry) obj.mReadyWrite.peek();
+                            if (!se.mAvailable) {
+                                obj.mReadyWrite.poll();
+                                continue;
+                            }
+
+                            /**说明该包还未被处理，下一轮继续判断*/
+                            if (se.mReadyWrite == null) {
+                                break;
+                            }
+
+                            ByteBuffer []bufs = se.mReadyWrite;
+
+                            for (int i=0;i<bufs.length;i++)
+                            {
+                                if (bufs[i]==null)
+                                {
+                                    if (i==bufs.length-1)
+                                    {
+                                        obj.mReadyWrite.poll();
+                                        afterWriteOne(obj,se);
+                                        continue write;
+                                    }
+                                }else
+                                {
+                                    /**一部分数据写入完成  继续*/
+                                    if (bufs[i].position()==bufs[i].limit())
+                                    {
+                                        mBufferPool.recycle(bufs[i]);
+                                        bufs[i]=null;
+                                    }else
+                                    {
+                                        try
+                                        {
+                                            /**实际转发*/
+                                            channel.write(bufs[i]);
+                                        }catch (Exception e)
+                                        {
+
+                                        }
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    } else {
+                        /**转发队列为空，说明有转发任务已经完成,把OP_WRITE去掉，不然key会保留*/
+                        key.interestOps((key.interestOps() | SelectionKey.OP_READ) & ~SelectionKey.OP_WRITE);
+                    }
+
                 }
                 if (key.isValid() && key.isReadable()) {
                     //read
+                    handleRead(channel,obj);
                 }
             } catch (CancelledKeyException e) {
             }
         }
     }
 
-    abstract void handleConnect();
+    abstract void afterConnect(T object);
 
-    abstract void handleWrite();
+    abstract void handleWrite(AbstractSelectableChannel channel,T object);
 
-    abstract void handleRead();
+    abstract void handleRead(AbstractSelectableChannel channel,T object);
+
+    abstract void afterWriteOne(T object,WriteEntry entry);
 
     class Key {
-        AbstractSelectableChannel mChannel;
+        SocketChannel mChannel;
         int mOp;
         T mObject;
 
-        Key(AbstractSelectableChannel c, int op, T o) {
+        Key(SocketChannel c, int op, T o) {
             mChannel=c;
             mOp = op;
             mObject=o;
+        }
+    }
+
+    public static class AttachmentObj <E extends WriteEntry>
+    {
+        Queue<E> mReadyWrite;
+        private ByteBuffer mReadBuffer;
+
+        public AttachmentObj(int readBuf,boolean needWrite)
+        {
+            if (readBuf>0)
+                mReadBuffer=ByteBuffer.allocate(readBuf);
+
+            if (needWrite)
+                mReadyWrite=new ConcurrentLinkedQueue<>();
+        }
+
+        public void addWrite(E src)
+        {
+            mReadyWrite.add(src);
+        }
+    }
+
+    public static class WriteEntry
+    {
+        ByteBuffer[] mReadyWrite = null;
+        boolean mAvailable = false;
+
+        public WriteEntry(boolean avai)
+        {
+            mAvailable=avai;
+        }
+
+        public void setSource(ByteBuffer[] src)
+        {
+            mReadyWrite=src;
         }
     }
 }
