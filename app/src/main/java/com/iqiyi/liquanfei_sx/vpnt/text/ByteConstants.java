de19fae6f8f09ea1dc7abf7bef5df6daa654ea99
Administrator <252539783@qq.com>
2017-11-14 21:10:19 +0800

    少了一个。。

--- app/src/main/java/com/iqiyi/liquanfei_sx/vpnt/text/ByteConstants.java
+++ app/src/main/java/com/iqiyi/liquanfei_sx/vpnt/text/ByteConstants.java
@@ -5,12 +5,12 @@ package com.iqiyi.liquanfei_sx.vpnt.text;
  */
 
 public class ByteConstants {
-    public static final String [] HEX=new String[0xff];
-    public static final String [] NUM=new String[0xff];
+    public static final String [] HEX=new String[0x100];
+    public static final String [] NUM=new String[0x100];
 
     public static void initHex()
     {
-        for (int i=0;i<0xff;i++)
+        for (int i=0;i<=0xff;i++)
         {
             HEX[i]=String.format("%02x",i);
         }
