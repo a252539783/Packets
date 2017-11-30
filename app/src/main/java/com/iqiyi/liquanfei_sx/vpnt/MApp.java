5c4b26792b6f38e2aecd61211f2ad0018732500a
Administrator <252539783@qq.com>
2017-11-30 20:01:51 +0800

    为了让Activity与悬浮窗的界面无缝连接而且能够直接重用，将通用View放置在Application中

--- app/src/main/java/com/iqiyi/liquanfei_sx/vpnt/MApp.java
+++ app/src/main/java/com/iqiyi/liquanfei_sx/vpnt/MApp.java
@@ -2,9 +2,20 @@ package com.iqiyi.liquanfei_sx.vpnt;
 
 import android.app.Application;
 import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
 
+import com.iqiyi.liquanfei_sx.vpnt.history.HistoryFragment2;
 import com.iqiyi.liquanfei_sx.vpnt.packet.LocalPackets;
+import com.iqiyi.liquanfei_sx.vpnt.saved.SavedFragment2;
+import com.iqiyi.liquanfei_sx.vpnt.tools.WeakLinkedList;
 import com.squareup.leakcanary.LeakCanary;
+
+import java.lang.ref.WeakReference;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by liquanfei_sx on 2017/8/14.
@@ -12,8 +23,16 @@ import com.squareup.leakcanary.LeakCanary;
 
 public class MApp extends Application {
 
+    public static final int RESOURCE_PACKET_PAGER=0;
+
+    private SparseArray<WeakLinkedList> mListeners=new SparseArray<>(1);
+
     private static MApp mInstance;
     private Handler mH=new Handler();
+
+    private ViewPager mPacketContent=null;
+
+    private MainPagerAdapter2 mAdapter;
 
     @Override
     public void onCreate() {
@@ -22,7 +41,32 @@ public class MApp extends Application {
         Constants.init(this);
         LocalPackets.mgr();
         LeakCanary.install(this);
-        //AppPortList.init();
+
+        mPacketContent= new ViewPager(this);
+        mAdapter=new MainPagerAdapter2(LayoutInflater.from(this),new PagerFragment[]{
+                new SavedFragment2(),
+                new HistoryFragment2()
+        },new String[]{
+                "saved",
+                "history"
+        });
+        mPacketContent.setAdapter(mAdapter);
+        mPacketContent.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+            @Override
+            public void onViewAttachedToWindow(View v) {
+                mAdapter.onAttach();
+            }
+
+            @Override
+            public void onViewDetachedFromWindow(View v) {
+                mAdapter.onDeAttach();
+            }
+        });
+    }
+
+    public View packetContent()
+    {
+        return mPacketContent;
     }
 
     public static MApp get()
@@ -33,5 +77,26 @@ public class MApp extends Application {
     public void postMain(Runnable r)
     {
         mH.post(r);
+    }
+
+    public void getResource(int requestCode,OnDispatchResourceListener l)
+    {
+        WeakLinkedList listeners=mListeners.get(requestCode);
+        if (listeners==null) {
+            listeners = new WeakLinkedList();
+            mListeners.put(requestCode,listeners);
+        }
+
+
+    }
+
+    public interface OnDispatchResourceListener
+    {
+        void onDispatch(int resourceCode,Object resource);
+    }
+
+    public interface OnRequestResourceListener
+    {
+        void onRequest(int resourceCode);
     }
 }
