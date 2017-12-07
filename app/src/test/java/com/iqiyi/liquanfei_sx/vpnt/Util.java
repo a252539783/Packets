b019de33a3ecb51ec6500792bdd3a999772e1831
Administrator <252539783@qq.com>
2017-12-07 13:01:47 +0800

    测试输出

---
+++ app/src/test/java/com/iqiyi/liquanfei_sx/vpnt/Util.java
@@ -0,0 +1,25 @@
+package com.iqiyi.liquanfei_sx.vpnt;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+public class Util {
+
+    public static void pln(int[] array) {
+        for (int i=0;i<array.length;i++)
+        {
+            p(String.format("%-10d",array[i]));
+        }
+        pln("");
+    }
+
+    public static void p(Object o) {
+        System.out.print(o);
+    }
+
+    public static void pln(Object o)
+    {
+        System.out.println(o);
+    }
+}
